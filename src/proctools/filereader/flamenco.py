7630eefb45c9ed76a1a511b3d5944452d4754906
Brad Pascoe <bradley.pascoe@unisq.edu.au>
2026-08-17 11:45:17 +1000

    Changes file loading to only take rmi_params

--- src/proctools/filereader/flamenco.py
+++ src/proctools/filereader/flamenco.py
@@ -1,128 +1,118 @@
 import os
+import numpy as np
 import pandas as pd
 from proctools.rmi import RMI_Parameters
 
 
-def load_integral_width_data(folder: str, time2tau: float = 1, wavelength: float = 1, rmi_parameters: RMI_Parameters = None) -> pd.DataFrame:
+def _time2tau_scaling(rmi_parameters: RMI_Parameters | None) -> float:
+    return rmi_parameters.time2tau if rmi_parameters is not None else 1.0
 
+
+def _wavelength_scaling(rmi_parameters: RMI_Parameters | None) -> float:
+    return rmi_parameters.lambda_bar if rmi_parameters is not None else 1.0
+
+
+def _tke_scaling(rmi_parameters: RMI_Parameters | None) -> float:
+    if rmi_parameters is None:
+        return 1.0
+    return 1.0/(0.5*rmi_parameters.rhoplus_bar*rmi_parameters.U0**2*rmi_parameters.lambda_bar*(2*np.pi)**2)
+
+
+def _omega_scaling(rmi_parameters: RMI_Parameters | None) -> float:
+    if rmi_parameters is None:
+        return 1.0
+    return 1.0/(rmi_parameters.rhoplus_bar*rmi_parameters.lambda_bar**(-2)*rmi_parameters.U0**2)
+
+
+def load_integral_width_data(folder: str) -> pd.DataFrame:
     integral_width_filename = "simulation_integral_width.dat"
     integral_width_cols = ["Time","W","Theta","Xi"]
     integral_width_file = os.path.join(folder,integral_width_filename)
-    integral_width_data = pd.read_fwf(integral_width_file,header=None,names=integral_width_cols,usecols=range(4))
-
-    if rmi_parameters is not None:
-        time2tau = rmi_parameters.time2tau
-        wavelength = rmi_parameters.lambda_bar
-
-    integral_width_data["Tau"] = integral_width_data["Time"]*time2tau
-    integral_width_data["WNorm"] = integral_width_data["W"]/wavelength
-    return integral_width_data
+    return pd.read_fwf(integral_width_file,header=None,names=integral_width_cols,usecols=range(4))
 
 
-def load_michael_iw_data(folder: str, time2tau: float = 1, wavelength: float = 1, rmi_parameters: RMI_Parameters = None) -> pd.DataFrame:
-
+def load_michael_iw_data(folder: str) -> pd.DataFrame:
     integral_width_filename = "simulation_integral_width.dat"
     integral_width_cols = ["Time","W","h","Theta","Xi"]
     integral_width_file = os.path.join(folder,integral_width_filename)
-    integral_width_data = pd.read_fwf(integral_width_file,header=None,names=integral_width_cols,usecols=range(5))
+    return pd.read_fwf(integral_width_file,header=None,names=integral_width_cols,usecols=range(5))
 
-    if rmi_parameters is not None:
-        time2tau = rmi_parameters.time2tau
-        wavelength = rmi_parameters.lambda_bar
 
-    integral_width_data["Tau"] = integral_width_data["Time"]*time2tau
-    integral_width_data["WNorm"] = integral_width_data["W"]/wavelength
+def nondimensionalise_integral_width(integral_width_data: pd.DataFrame, rmi_parameters: RMI_Parameters | None = None) -> pd.DataFrame:
+    integral_width_data["Tau"] = integral_width_data["Time"]*_time2tau_scaling(rmi_parameters)
+    integral_width_data["WNorm"] = integral_width_data["W"]/_wavelength_scaling(rmi_parameters)
     return integral_width_data
 
 
-def load_tke_data(folder: str, time2tau: float = 1, tke_scaling: float = 1, rmi_parameters: RMI_Parameters = None) -> pd.DataFrame:
-    import numpy as np
-
-    if rmi_parameters is not None:
-        time2tau = rmi_parameters.time2tau
-        tke_scaling = 1.0/(0.5*rmi_parameters.rhoplus_bar*rmi_parameters.U0**2*rmi_parameters.lambda_bar*(2*np.pi)**2)
-
+def load_tke_data(folder: str) -> pd.DataFrame:
     tke_filename = "simulation_tke.dat"
     tke_cols = ["Time","TKE","TKX","TKY","TKZ"]
     tke_file = os.path.join(folder,tke_filename)
-    tke_data = pd.read_fwf(tke_file,header=None,names=tke_cols,usecols=range(5))
-    tke_data['Tau'] = tke_data['Time']*time2tau
-    tke_data['TKR'] = 2*tke_data['TKX']/(tke_data['TKY']+tke_data['TKZ'])
-    tke_data['TKX'] *= tke_scaling
-    tke_data['TKY'] *= tke_scaling
-    tke_data['TKZ'] *= tke_scaling
-    tke_data['TKE'] *= tke_scaling
-
-    return tke_data
+    return pd.read_fwf(tke_file,header=None,names=tke_cols,usecols=range(5))
 
 
-def load_michael_tke_data(folder: str, time2tau: float = 1, tke_scaling: float = 1, rmi_parameters: RMI_Parameters = None) -> pd.DataFrame:
-    import numpy as np
-
-    if rmi_parameters is not None:
-        time2tau = rmi_parameters.time2tau
-        tke_scaling = 1.0/(0.5*rmi_parameters.rhoplus_bar*rmi_parameters.U0**2*rmi_parameters.lambda_bar*(2*np.pi)**2)
-
+def load_michael_tke_data(folder: str) -> pd.DataFrame:
     tke_filename = "simulation_integral_ke.dat"
     tke_cols = ["Time","TKE","TKX","TKY","TKZ"]
     tke_file = os.path.join(folder,tke_filename)
-    tke_data = pd.read_fwf(tke_file,header=None,names=tke_cols,usecols=range(5))
-    tke_data['Tau'] = tke_data['Time']*time2tau
-    tke_data['TKR'] = 2*tke_data['TKX']/(tke_data['TKY']+tke_data['TKZ'])
-    tke_data['TKX'] *= tke_scaling
-    tke_data['TKY'] *= tke_scaling
-    tke_data['TKZ'] *= tke_scaling
-    tke_data['TKE'] *= tke_scaling
+    return pd.read_fwf(tke_file,header=None,names=tke_cols,usecols=range(5))
 
+
+def nondimensionalise_tke(tke_data: pd.DataFrame, rmi_parameters: RMI_Parameters | None = None) -> pd.DataFrame:
+    tke_data['Tau'] = tke_data['Time']*_time2tau_scaling(rmi_parameters)
+    tke_data['TKR'] = 2*tke_data['TKX']/(tke_data['TKY']+tke_data['TKZ'])
+    scaling = _tke_scaling(rmi_parameters)
+    tke_data['TKX'] *= scaling
+    tke_data['TKY'] *= scaling
+    tke_data['TKZ'] *= scaling
+    tke_data['TKE'] *= scaling
     return tke_data
 
 
-def load_vorticity_data(folder: str, time2tau: float = 1, omega_scaling: float = 1,rmi_params: RMI_Parameters = None) -> pd.DataFrame:
-
-    if rmi_params is not None:
-        time2tau = rmi_params.time2tau
-        omega_scaling = 1.0/(rmi_params.rhoplus_bar*rmi_params.lambda_bar**(-2)*rmi_params.U0**2)
-
+def load_vorticity_data(folder: str) -> pd.DataFrame:
     vorticity_filename = "simulation_omega.dat"
     vorticity_cols = ["Time","Omega","OmegaX","OmegaY","OmegaZ"]
     vorticity_file = os.path.join(folder,vorticity_filename)
-    vorticity_data = pd.read_fwf(vorticity_file,header=None,names=vorticity_cols,usecols=range(5))
-    vorticity_data['Tau'] = vorticity_data['Time']*time2tau
-    vorticity_data['Omega'] *= omega_scaling
-    vorticity_data['OmegaX'] *= omega_scaling
-    vorticity_data['OmegaY'] *= omega_scaling
-    vorticity_data['OmegaZ'] *= omega_scaling
+    return pd.read_fwf(vorticity_file,header=None,names=vorticity_cols,usecols=range(5))
+
+
+def nondimensionalise_vorticity(vorticity_data: pd.DataFrame, rmi_parameters: RMI_Parameters | None = None) -> pd.DataFrame:
+    vorticity_data['Tau'] = vorticity_data['Time']*_time2tau_scaling(rmi_parameters)
+    scaling = _omega_scaling(rmi_parameters)
+    vorticity_data['Omega'] *= scaling
+    vorticity_data['OmegaX'] *= scaling
+    vorticity_data['OmegaY'] *= scaling
+    vorticity_data['OmegaZ'] *= scaling
     return vorticity_data
 
 
-def load_mix_limits_data(folder: str, time2tau: float = 1, wavelength: float = 1, rmi_params: RMI_Parameters = None) -> pd.DataFrame:
-    if rmi_params is not None:
-        wavelength = rmi_params.lambda_bar
-        time2tau = rmi_params.time2tau
-
+def load_mix_limits_data(folder: str) -> pd.DataFrame:
     mix_filename = "simulation_mix_limits.dat"
     mix_cols = ["Time","Xmin","Xmax","XDiff","XCentre"]
     mix_file = os.path.join(folder,mix_filename)
     mix_data = pd.read_fwf(mix_file,header=None,names=mix_cols,usecols=range(5))
     mix_data["Amplitude"] = mix_data["XDiff"]/2.0
-    mix_data["AmpNorm"] = mix_data["Amplitude"]/wavelength
-    mix_data["Tau"] = mix_data["Time"]*time2tau
-
     return mix_data
 
 
-def load_bubblespike_data(folder: str, time2tau: float = 1, wavelength: float = 1, rmi_parameters: RMI_Parameters = None) -> pd.DataFrame:
+def nondimensionalise_mix_limits(mix_data: pd.DataFrame, rmi_parameters: RMI_Parameters | None = None) -> pd.DataFrame:
+    mix_data["AmpNorm"] = mix_data["Amplitude"]/_wavelength_scaling(rmi_parameters)
+    mix_data["Tau"] = mix_data["Time"]*_time2tau_scaling(rmi_parameters)
+    return mix_data
 
-    if rmi_parameters is not None:
-        time2tau = rmi_parameters.time2tau
-        wavelength = rmi_parameters.lambda_bar
 
+def load_bubblespike_data(folder: str) -> pd.DataFrame:
     bubblespike_filename = "simulation_bubblespike.dat"
     bubblespike_cols = ["Time", "XCentre","Hb","Hs"]
     bubblespike_file = os.path.join(folder,bubblespike_filename)
     bubblespike_data = pd.read_fwf(bubblespike_file,header=None,names=bubblespike_cols)
-    bubblespike_data["Tau"] = bubblespike_data["Time"]*time2tau
     bubblespike_data["Ratio"] = bubblespike_data["Hs"]/bubblespike_data["Hb"]
+    return bubblespike_data
+
+
+def nondimensionalise_bubblespike(bubblespike_data: pd.DataFrame, rmi_parameters: RMI_Parameters | None = None) -> pd.DataFrame:
+    bubblespike_data["Tau"] = bubblespike_data["Time"]*_time2tau_scaling(rmi_parameters)
+    wavelength = _wavelength_scaling(rmi_parameters)
     bubblespike_data['Hb'] /= wavelength
     bubblespike_data['Hs'] /= wavelength
     bubblespike_data['H'] = bubblespike_data['Hb']+bubblespike_data['Hs']
--- src/proctools/filereader/postprocessing.py
+++ src/proctools/filereader/postprocessing.py
@@ -1,8 +1,37 @@
 import os
+import math
 import pandas as pd
 import re
 from proctools.filereader.custom import load_tecplot
 from proctools.rmi import RMI_Parameters
+
+
+def _time2tau_scaling(rmi_parameters: RMI_Parameters | None) -> float:
+    return rmi_parameters.time2tau if rmi_parameters is not None else 1.0
+
+
+def _wavelength_scaling(rmi_parameters: RMI_Parameters | None) -> float:
+    return rmi_parameters.lambda_bar if rmi_parameters is not None else 1.0
+
+
+def _mixed_mass_scaling(rmi_parameters: RMI_Parameters | None) -> float:
+    if rmi_parameters is None:
+        return 1.0
+    return 1.0/(4.0*rmi_parameters.rhoplus_bar*rmi_parameters.lambda_bar*(2.0*math.pi)**2)
+
+
+def _ke_scaling(rmi_parameters: RMI_Parameters | None) -> float:
+    if rmi_parameters is None:
+        return 1.0
+    return 1.0/(0.5*rmi_parameters.rhoplus_bar*rmi_parameters.U0**2)
+
+
+def _kbar_scaling(rmi_parameters: RMI_Parameters | None) -> float:
+    return rmi_parameters.kbar if rmi_parameters is not None else 1.0
+
+
+def _stress_scaling(rmi_parameters: RMI_Parameters | None) -> float:
+    return 1.0/rmi_parameters.U0**2 if rmi_parameters is not None else 1.0
 
 
 def load_planar_average_tecdata(folder,time):
@@ -18,66 +47,67 @@ def load_planar_average_tecdata(folder,time):
     raise FileNotFoundError
 
 
-def load_results_cross(folder: str,RMI: RMI_Parameters = None) -> pd.DataFrame:
+def load_results_cross(folder: str) -> pd.DataFrame:
     results_cross_filename = "results_cross.dat"
     results_cross_cols = ["Time","XCross","TransverseL","TransverseLAlt"]
     results_cross_file = os.path.join(folder,results_cross_filename)
-    results_cross_data = pd.read_fwf(results_cross_file,header=None,names=results_cross_cols,usecols=range(3))
+    return pd.read_fwf(results_cross_file,header=None,names=results_cross_cols,usecols=range(3))
 
-    if RMI is not None:
-        results_cross_data['Tau'] = results_cross_data['Time']*RMI.time2tau
-        results_cross_data['L'] = results_cross_data['TransverseL']/RMI.lambda_bar
+
+def nondimensionalise_results_cross(results_cross_data: pd.DataFrame, rmi_parameters: RMI_Parameters | None = None) -> pd.DataFrame:
+    results_cross_data['Tau'] = results_cross_data['Time']*_time2tau_scaling(rmi_parameters)
+    results_cross_data['L'] = results_cross_data['TransverseL']/_wavelength_scaling(rmi_parameters)
     return results_cross_data
 
 
-def load_mixed_mass(folder: str, time2tau: float = 1, mixedMassNonDim: float = 1.0, rmi_params: RMI_Parameters = None) -> pd.DataFrame:
-    import math
-
-    if rmi_params is not None:
-        time2tau = rmi_params.time2tau
-        # mixedMassNonDim = 1.0/(rmi_params.rhoplus_bar*rmi_params.lambda_bar*(2.0*math.pi)**2)
-        mixedMassNonDim = 1.0/(4.0*rmi_params.rhoplus_bar*rmi_params.lambda_bar*(2.0*math.pi)**2)
+def load_mixed_mass(folder: str) -> pd.DataFrame:
     mixed_mass_filename = "results_mixed_mass.dat"
     mixed_mass_cols = ["Time", "MixedMass","Psi"]
     mixed_mass_file = os.path.join(folder,mixed_mass_filename)
-    mixed_mass_data = pd.read_fwf(mixed_mass_file,header=None,names=mixed_mass_cols)
-    mixed_mass_data["Tau"] = mixed_mass_data["Time"]*time2tau
-    mixed_mass_data["MixedMass"] *= mixedMassNonDim
+    return pd.read_fwf(mixed_mass_file,header=None,names=mixed_mass_cols)
+
+
+def nondimensionalise_mixed_mass(mixed_mass_data: pd.DataFrame, rmi_parameters: RMI_Parameters | None = None) -> pd.DataFrame:
+    mixed_mass_data["Tau"] = mixed_mass_data["Time"]*_time2tau_scaling(rmi_parameters)
+    mixed_mass_data["MixedMass"] *= _mixed_mass_scaling(rmi_parameters)
     return mixed_mass_data
 
 
-def load_spectrum_data(file: str, rmi_params: RMI_Parameters = None) -> pd.DataFrame:
-    from math import pi
+def load_spectrum_data(file: str) -> pd.DataFrame:
     spectrum_cols = ["X", "Wavenumber","Ex","Ey","Ez"]
     spectrum_data = pd.read_fwf(file, header=None, names=spectrum_cols)
     spectrum_data['Transverse'] = 0.5*(spectrum_data['Ey']+spectrum_data['Ez'])
     spectrum_data['Anisotropy'] = spectrum_data['Ex']/spectrum_data['Transverse']
-
-    if rmi_params is not None:
-        KE_nondim = 1.0/(0.5*rmi_params.rhoplus_bar*rmi_params.U0**2)  # *rmi_params.lambda_bar*(2*pi)**2
-        spectrum_data['Ex'] *= KE_nondim
-        spectrum_data['Ey'] *= KE_nondim
-        spectrum_data['Ez'] *= KE_nondim
-        spectrum_data['Transverse'] *= KE_nondim
-        spectrum_data['Wavenumber'] /= rmi_params.kbar
     return spectrum_data
 
 
-def load_decomposed_spectrum_data(file: str, rmi_params: RMI_Parameters = None) -> pd.DataFrame:
+def nondimensionalise_spectrum(spectrum_data: pd.DataFrame, rmi_parameters: RMI_Parameters | None = None) -> pd.DataFrame:
+    ke_scaling = _ke_scaling(rmi_parameters)
+    spectrum_data['Ex'] *= ke_scaling
+    spectrum_data['Ey'] *= ke_scaling
+    spectrum_data['Ez'] *= ke_scaling
+    spectrum_data['Transverse'] *= ke_scaling
+    spectrum_data['Wavenumber'] /= _kbar_scaling(rmi_parameters)
+    return spectrum_data
+
+
+def load_decomposed_spectrum_data(file: str) -> pd.DataFrame:
     spectrum_cols = ["Wavenumber","Ey","Ez","EyD","EzD","EyS","EzS"]
     spectrum_data = pd.read_fwf(file, header=None, names=spectrum_cols)
     spectrum_data['Transverse'] = 0.5*(spectrum_data['Ey']+spectrum_data['Ez'])
     spectrum_data['Dilatational'] = 0.5*(spectrum_data['EyD'] + spectrum_data['EzD'])
     spectrum_data['Solenoidal'] = 0.5*(spectrum_data['EyS'] + spectrum_data['EzS'])
+    return spectrum_data
 
-    if rmi_params is not None:
-        KE_nondim = 1.0/(0.5*rmi_params.rhoplus_bar*rmi_params.U0**2)  # *rmi_params.lambda_bar*(2*pi)**2
-        spectrum_data['Ey'] *= KE_nondim
-        spectrum_data['Ez'] *= KE_nondim
-        spectrum_data['Transverse'] *= KE_nondim
-        spectrum_data['Dilatational'] *= KE_nondim
-        spectrum_data['Solenoidal'] *= KE_nondim
-        spectrum_data['Wavenumber'] /= rmi_params.kbar
+
+def nondimensionalise_decomposed_spectrum(spectrum_data: pd.DataFrame, rmi_parameters: RMI_Parameters | None = None) -> pd.DataFrame:
+    ke_scaling = _ke_scaling(rmi_parameters)
+    spectrum_data['Ey'] *= ke_scaling
+    spectrum_data['Ez'] *= ke_scaling
+    spectrum_data['Transverse'] *= ke_scaling
+    spectrum_data['Dilatational'] *= ke_scaling
+    spectrum_data['Solenoidal'] *= ke_scaling
+    spectrum_data['Wavenumber'] /= _kbar_scaling(rmi_parameters)
     return spectrum_data
 
 
@@ -137,16 +167,14 @@ def load_tke_data(folder: str, time2tau: float = 1, tke_scaling: float = 1) -> p
     return tke_data
 
 
-def load_favre_stress_data(case_folder: str, time: float, time2tau: float = 1, stress_scaling: float = 1, rmi_param: RMI_Parameters = None) -> pd.DataFrame:
-
-    if rmi_param is not None:
-        time2tau = rmi_param.time2tau
-        stress_scaling = 1.0/rmi_param.U0**2
+def load_favre_stress_data(case_folder: str, time: float) -> pd.DataFrame:
     rs_file = os.path.join(case_folder,"ReynoldsStress",f"{time:.8f}_Favre.dat")
-    rs_cols = ["Time","u1u1","u2u2","u3u3","u1u2","u1u3","u2u3"]
-    rs_data = pd.read_fwf(rs_file,usecols=range(7))
-    # rs_data = pd.read_fwf(rs_file,header=None,names=rs_cols,usecols=range(7))
-    # rs_data['Tau'] = rs_data['Time']*time2tau
-    for col in rs_cols[1:]:
+    return pd.read_fwf(rs_file,usecols=range(7))
+
+
+def nondimensionalise_favre_stress(rs_data: pd.DataFrame, rmi_parameters: RMI_Parameters | None = None) -> pd.DataFrame:
+    stress_scaling = _stress_scaling(rmi_parameters)
+    stress_cols = ["u1u1","u2u2","u3u3","u1u2","u1u3","u2u3"]
+    for col in stress_cols:
         rs_data[col] *= stress_scaling
     return rs_data
